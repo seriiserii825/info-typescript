6122b1163be89ff95c8258f468068bb54437f3e6
serii <seriiburduja@gmail.com>
2024-08-10 13:32:05 +0300

    upd: 03

--- 03.generics.ts
+++ 03.generics.ts
@@ -8,20 +8,55 @@ interface IMessage {
   text: string;
 }
 
-interface IUserState {
+interface IState<T> {
   loading: boolean;
   error: Error | null;
-  data: IUser;
+  data: T;
 }
 
-interface IMessageState {
-  loading: boolean;
-  error: Error | null;
-  data: IMessage;
+type IUserState = IState<IUser>;
+
+type IMessageState = IState<IMessage>;
+
+const message_state: IMessageState = {
+  data: {
+    id: 1,
+    text: "Hello",
+  },
+  loading: false,
+  error: null,
+};
+
+// random element ====================================================
+function getRandomElement<T>(items: T[]): T {
+  return items[Math.floor(Math.random() * items.length)];
 }
 
-function func1<T>(n: T) {
-  return n;
+const numbers = [1, 2, 3, 4, 5];
+const random_number = getRandomElement(numbers);
+
+// generic promise ====================================================
+async function fakeRequest() {
+  return 2;
 }
 
-const func1_const = func1(10);
+const request: Promise<number> = fakeRequest();
+
+//generic object ====================================================
+type IObj = {
+  [key: string]: number;
+};
+
+// or
+type IObj2 = Record<string, number>;
+
+// funciton params ====================================================
+function len<T extends {length: number}>(collection: T): number {
+  return collection.length;
+}
+
+const r1 = len([1, 2, 3]);
+const r2 = len("hello");
+const r3 = len(3); // error
+
+
